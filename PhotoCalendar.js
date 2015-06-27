547ae436d54c9449f2a68b3aa0acbb7d57586c28
Joy Gao <joyg@wepay.com>
2015-06-27 09:52:33 -0700

    fixed setup

--- PhotoCalendar.js
+++ PhotoCalendar.js
@@ -2,31 +2,18 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
-  Meteor.publish('calendar', function () {
-    return Calendar.find();
-  });
+  Meteor.subscribe('calendar', function () {
+  Session.set('superCalendarReady', true);
+});
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-  
-  Meteor.subscribe('calendar', function () {
-    Session.set('superCalendarReady', true);
-  });
+
+    Meteor.publish('calendar', function () {
+  return Calendar.find();
+});
 
 }
